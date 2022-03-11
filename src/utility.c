3b258d57a1101031205bb5436be40d1e81ac31e6
DuarteMartinho <duartemartinho11@gmail.com>
2022-03-11 00:35:21 +0000

    Utility file comments

--- src/utility.c
+++ src/utility.c
@@ -14,13 +14,57 @@
 #define MAX_ARGS 64 // max # args
 #define SEPARATORS " \t\n" // token separators
 
+/*
+ * Function:  setInitialEnv
+ * --------------------
+ * Args: 
+ *      1 - buf : line buffer
+ * 
+ * Purpose: 
+ *      The purpose of this function is to set the inital environment variables
+ * 
+ */
+void setInitialEnv(char *buf) {
+    char *currdir = getcwd(buf, MAX_BUFFER); // Gets current directory
+    char *shell = calloc(strlen(currdir) + 20, sizeof(char)); // Allocates memory to shell variable 
+    strcat(shell, "SHELL="); // Adds SHELL to shell variable
+    strcat(shell, currdir); // Adds current directory path to shell variable
+    strcat(shell, "/myshell"); // Adds shell name to shell variable
+    putenv(shell); // Puts shell variable into SHELL env
+    char *shellpath = calloc(strlen(currdir) + 20, sizeof(char)); // Allocates memory to shellpath variable 
+    strcat(shellpath, "SHELLPATH="); // Adds SSHELLPATH to shellpath variable 
+    strcat(shellpath, currdir); // Adds current directory path to shellpath
+    putenv(shellpath); // Puts shellpath variable into SHELLPATH env
+}
+
+/*
+ * Function:  myprompt
+ * --------------------
+ * Args: 
+ *      1 - buf : line buffer
+ * 
+ * Purpose: 
+ *      The purpose of this function is to print out the prompt with current directory
+ * 
+ */
 void myprompt(char *buf) {
     char * prompt = "$ " ; // Defines prompt
     char *currdir = getcwd(buf, MAX_BUFFER); // Gets current working directory
     fprintf(stdout, "%s\n%s", currdir, prompt); // Prints out to screen the prompt
 }
 
-
+/*
+ * Function:  inputstoargs
+ * --------------------
+ * Args: 
+ *      1 - input : line of input
+ *      2 - size : pointer to size of input after split
+ * 
+ * Purpose: 
+ *      The purpose of this function is to split the line of input and to count its number of args 
+ *      Returns the line of input splitted and uses the pointer to return the size of the array
+ * 
+ */
 char ** inputstoargs(char * input, int *size) {
     int i = 0; // Sets initial position
     char *arg; // Pointer for args
